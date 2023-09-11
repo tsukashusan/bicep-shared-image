48bc0ae36de65f9d32723d4e543a328c2661538a
tsukabon <shtsukam@outlook.com>
2023-09-12 08:51:51 +0900

    update.

--- org_deploy.ps1
+++ org_deploy.ps1
@@ -1,10 +1,10 @@
-set-variable -name TENANT_ID "16b3c013-d300-468d-ac64-7eda0820b6d3" -option constant
-set-variable -name SUBSCRIPTOIN_GUID "be7d3851-09f6-433c-8da5-09aace58dcd2" -option constant
+set-variable -name TENANT_ID "<TENANT_ID>" -option constant
+set-variable -name SUBSCRIPTOIN_GUID "<SUBSCRIPTOIN_GUID>" -option constant
 
 $bicepFile = "main.bicep"
-$parameterFile = "azuredeploy.parameters.dev4devClient.json"
-$resourceGroupName = "shared-image"
-$moduleame = "createUbuntu224DevImage"
+$parameterFile = "<parameterFile>"
+$resourceGroupName = "<resourceGroupName>"
+$moduleame = "<moduleame>"
 
 Connect-AzAccount -Tenant ${TENANT_ID} -Subscription ${SUBSCRIPTOIN_GUID}
 
